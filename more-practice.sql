07c15546974f8ac60073d931cc4c1e9f2b3f4080
Agnė Klimaitė <agnaite@me.com>
2016-07-30 12:17:06 -0700

    finished more-practice

--- more-practice.sql
+++ more-practice.sql
@@ -3,9 +3,22 @@
 
 
 -- INSERT
+INSERT INTO models (year, brand_name, name)
+    VALUES (2015, 'Chevrolet', 'Malibu');
 
+INSERT INTO models (year, brand_name, name)
+    VALUES (2015, 'Subaru', 'Outback');
 
 -- CREATE TABLE
-
+CREATE TABLE awards (
+    winner_id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    year INTEGER NOT NULL
+);
 
 -- More INSERT
+INSERT INTO awards (winner_id, name, year)
+    VALUES (49, 'IIHS Safety Award Year', 2015);
+
+INSERT INTO awards (winner_id, name, year)
+    VALUES (50, 'IIHS Safety Award Year', 2015);
